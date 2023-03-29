0c4fc58547089a6c15262c1ba826834f912ffc32
mohamed shames <mohamedhames900@gmail.com>
2023-03-29 20:35:18 +0200

    update

--- script/script.js
+++ script/script.js
@@ -17,4 +17,17 @@ close.addEventListener("click",(eo) => {
 window.onscroll = () => {
     navbar.classList.remove("active");
     close.style.display = "none";
+    yourCarts.classList.remove("active");
 }
+
+let carts = document.getElementById("carts");
+let yourCarts = document.querySelector('.your-carts');
+let closeCart = document.querySelector('#close-cart');
+carts.addEventListener("click", (eo) => {
+    yourCarts.classList.add("active");
+})
+
+closeCart.addEventListener("click", (eo) => {
+    yourCarts.classList.remove("active");
+})
+
